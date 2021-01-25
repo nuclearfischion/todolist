3e2faef172487b25af00308df96fa4f9f598aa7c
Cheryl <cherylfischer11@gmail.com>
2021-01-24 18:18:50 -0600

    added deleteTodo function to trash icon for each todo item

--- public/app.js
+++ public/app.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
             console.log("todoField: ", newTodoInput);
             createTodo(newTodoInput);
 
-            $("#todoField").val("");
         }
     });
 });
@@ -21,66 +20,96 @@ function addTodos(){
     // make jQuery request
     console.log("adding your todos...");
     $.ajax(url, {'method': 'GET'})
-        .done(function(res){
-            console.log("result is: ", res);
-            res.forEach((e, i)=>{
-                let id = e._id;
-                console.log(id);
-
-                addTodo(id, e);
-
-            });
+    .done(function(res){
+        console.log("result is: ", res);
+        res.forEach((e, i)=>{
+            console.log(e);
+            
+            addTodo(e);
+            
         });
+    });
 }
 
 function createTodo(newTodo){
     // sent request to create new todo
     $.post(url, {"name": newTodo})
-        .then(function(newTodo){
-            console.log("sent post request");
-            console.log('newTodo: ', newTodo);
-
+    .then(function(newTodo){
+        console.log("sent post request");
+        console.log('newTodo: ', newTodo);
+        
             //append todo to HTML
-            addTodo(newTodo._id, newTodo);
+            addTodo(newTodo);
+            $("#todoField").val("");    //clear input box
         })
         .catch(function(err){
             console.log(err);
         });
 }
 
-function addTodo(id, e){
+function deleteTodo(todoItem){
+    console.log("gonna delete your todo!");
+
+    // sent request to create new todo
+    $.ajax({url: url+"/"+todoItem, method: 'DELETE'})
+    .then(function(newTodo){
+        console.log("sent delete request");
+        console.log('newTodo: ', newTodo);
+        
+        //     //append todo to HTML
+        //     deleteTodo(newTodo);
+        //     // $("#todoField").val("");    //clear input box
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+}
+
+function addTodo(todoItem){
+    console.log("****addTodo item: ", todoItem._id);
     // build html element
     let listItemElement = document.createElement("li");
+    $(listItemElement).data('id', todoItem._id);
+    console.log("$$$$listItemElement.data: ", $(listItemElement).data());
     $(listItemElement).addClass("collection-item");
     
     let labelElement = document.createElement("label");
     let inputElement = document.createElement("input");
+    let deleteButton = document.createElement("button");
+    $(deleteButton).addClass("button-delete");
+    $(deleteButton).html("<i class=\"material-icons small delete\">delete</i>");
+    $(deleteButton).click(function(){
+        let clickedItemId = $(this).parent().data().id;
+        deleteTodo(clickedItemId);
+    });
     // check if element is completed
     // console.log(e);
     
-    $(inputElement).attr("type", "checkbox").addClass("filled-in").attr("id", id);
+    $(inputElement).attr("type", "checkbox").addClass("filled-in").attr("id", todoItem._id);
     
     let spanElement = document.createElement("span")
-    $(spanElement).html(e.name);
+    $(spanElement).html(todoItem.name);
     
-    if(e.completed){
+    if(todoItem.completed){
         $(inputElement).attr("checked", "");
-        $(spanElement).addClass("done");
+        $(spanElement).addClass("strikethrough");
     }
     else{
         $(inputElement).removeAttr("checked");
-        $(spanElement).removeClass("done");
+        $(spanElement).removeClass("strikethrough");
     }
 
     //append an input to label
     $(labelElement).append(inputElement);
     //append a span with todo name to label
     $(labelElement).append(spanElement);
-
+    
     //append label to li
     $(listItemElement).append(labelElement);
     // console.log(result);
+    $(listItemElement).append(deleteButton);
     $(".list").append(listItemElement);
     // $(".list").append(result);
     console.log(labelElement);
 }
+
